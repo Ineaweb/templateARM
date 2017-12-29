7b5f70fd4aa36ac88813271c9b565144b51518d1
Philippe MORISSEAU <philippe.morisseau@outlook.com>
2017-12-29 16:41:58 +0100

    Correction 30

--- inventory/UpdateInventory.py
+++ inventory/UpdateInventory.py
@@ -12,6 +12,7 @@ def main():
     with open('/var/lib/awx/projects/_6__tsf_azure/inventory/RedisInventory.json', 'r') as content_file:
         content = content_file.read()
         print(content)
+        
     with open('/var/lib/awx/projects/_6__tsf_azure/inventory/webAppInventory.json', 'r') as content_file:
         content = content_file.read()
         print(content)        
