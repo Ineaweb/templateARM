73c2301d411753caa0473919f0fda9fd647e3f81
Philippe MORISSEAU <philippe.morisseau@outlook.com>
2017-12-29 16:48:45 +0100

    Correction 32

--- inventory/UpdateInventory.py
+++ inventory/UpdateInventory.py
@@ -13,8 +13,8 @@ def main():
         content = content_file.read()
         print(content)
 
-    with open('/var/lib/awx/projects/_6__tsf_azure/inventory/webAppInventory.json', 'r') as content_file:
-        content = content_file.read()
-        print(content)
+#    with open('/var/lib/awx/projects/_6__tsf_azure/inventory/webAppInventory.json', 'r') as content_file:
+#        content = content_file.read()
+#        print(content)
 
 main()
