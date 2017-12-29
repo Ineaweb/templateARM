6102da84ecba198f0c3a85007335dc08e54d4db0
Philippe MORISSEAU <philippe.morisseau@outlook.com>
2017-12-29 16:11:34 +0100

    Correction 25

--- inventory/UpdateInventory.py
+++ inventory/UpdateInventory.py
@@ -9,13 +9,13 @@ import sys
 import inspect
 
 def main():
-	cwd = os.getcwd()
-	webapppath = "{0}/webAppInventory.json".format(cwd)
-	redispath = "{0}/RedisInventory.json".format(cwd)
+    cwd = os.getcwd()
+    webapppath = "{0}/webAppInventory.json".format(cwd)
+    redispath = "{0}/RedisInventory.json".format(cwd)
     with open(webapppath, 'r') as webapp_file:
         webapp = webapp_file.read()
         print(webapp)		
-	with open(redispath, 'r') as redis_file:
+    with open(redispath, 'r') as redis_file:
         redis = redis_file.read()
         print(redis)	
 
