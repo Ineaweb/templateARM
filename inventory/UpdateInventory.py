94c9cee731271fad44ab63212caf6094b81cbc27
Philippe MORISSEAU <philippe.morisseau@outlook.com>
2017-12-29 16:39:02 +0100

    UpdateInventory

--- inventory/UpdateInventory.py
+++ inventory/UpdateInventory.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 # Python
+import argparse
 import json
 import os
 import re
@@ -8,11 +9,11 @@ import sys
 import inspect
 
 def main():
-    with open('/var/lib/awx/projects/_6__tsf_azure/inventory/webAppInventory.json', 'r') as webapp_file:
-        webapp = webapp_file.read()
-        print(webapp)		
-    with open('/var/lib/awx/projects/_6__tsf_azure/inventory/RedisInventory.json', 'r') as redis_file:
-        redis = redis_file.read()
-        print(redis)	
+    with open('/var/lib/awx/projects/_6__tsf_azure/inventory/RedisInventory.json', 'r') as content_file:
+        content = content_file.read()
+        print(content)
+    with open('/var/lib/awx/projects/_6__tsf_azure/inventory/webAppInventory.json', 'r') as content_file:
+        content = content_file.read()
+        print(content)        
 
 main()
