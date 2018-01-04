e99d88ba11fb71f938e09c4c15c5e96c2969ad9f
Philippe MORISSEAU <philippe.morisseau@outlook.com>
2018-01-04 11:26:46 +0100

    Correction 51

--- inventory/UpdateInventory.py
+++ inventory/UpdateInventory.py
@@ -9,7 +9,7 @@ import sys
 import inspect
 
 def main():
-    with open('/var/lib/awx/projects/_6__tsf_azure/inventory/fullInventory.json', 'r') as content_file:
+    with open('/var/lib/awx/projects/_6__z_aim_pmo3_wso_ew1_dev/inventory/fullInventory.json', 'r') as content_file:
         content = content_file.read()
         print(content)
 
